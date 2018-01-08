f6fd532ded64d7dad32561177cd5326b37d98bc4
Lushang <tyler@lushang.me>
2018-01-08 14:03:58 +0800

    add 10 new component completion reference
    update the fetcher

--- python/lightning.py
+++ python/lightning.py
@@ -30,29 +30,33 @@ def get_attribute(_url_key):
     cmp = json.loads(r.text, encoding=r.encoding)
     attr_page = BeautifulSoup(cmp['content'], 'lxml')
     tables = attr_page.find_all('table')
-    trs = tables[-1].find_all('tr')
-    for tr in trs[1:]:
-        tds = tr.find_all('td')
-        # attribute = {'Attribute': tds[0].text, 'Type': tds[1].text, 'Description': tds[2].text}
-        # attributes.append(attribute)
-        if len(tds) >= 3:
-            description = tds[2].text
-            description = re.sub(r'\n(\s)+', ' ', description)
-            des_list = description.split('.')
-            if len(des_list) > 2 and len(description) > 200:
-                description = des_list[0]
-            attribute['attribs'][tds[0].text.replace("\n", "")] = {"type": tds[1].text, "description": description}
-        else:
-            print(_url_key)
+    for table in tables:
+        trs = table.find_all('tr')
+        for tr in trs[1:]:
+            tds = tr.find_all('td')
+            # attribute = {'Attribute': tds[0].text, 'Type': tds[1].text, 'Description': tds[2].text}
+            # attributes.append(attribute)
+            if len(tds) >= 3:
+                description = tds[2].text
+                description = re.sub(r'\n(\s)+', ' ', description)
+                des_list = description.split('.')
+                if len(des_list) > 2 and len(description) > 200:
+                    description = des_list[0]
+                attribute['attribs'][tds[0].text.replace("\n", "")] = {"type": tds[1].text, "description": description}
+            else:
+                print(_url_key)
     return attribute
 
 
 ref_prefix = 'aura_compref_'
-aura = 'lightning:accordion,lightning:accordionSection,lightning:breadcrumb,lightning:buttonIconStateful,lightning:checkboxGroup,lightning:clickToDial,lightning:combobox,lightning:datatable,lightning:dualListbox,lightning:dynamicIcon,lightning:fileCard,lightning:fileUpload,lightning:flexipageRegionInfo,lightning:flow,lightning:formattedEmail,lightning:formattedLocation,lightning:formattedPhone,lightning:formattedRichText,lightning:formattedText,lightning:formattedUrl,lightning:helptext,lightning:inputLocation,lightning:omniToolkitAPI,lightning:outputField,lightning:path,lightning:picklistPath,lightning:progressBar,lightning:progressIndicator,lightning:radioGroup,lightning:recordViewForm,lightning:slider,lightning:tree,lightning:utilityBarAPI,lightning:verticalNavigation,lightning:verticalNavigationItem,lightning:verticalNavigationItemBadge,lightning:verticalNavigationItemIcon,lightning:verticalNavigationOverflow,lightning:verticalNavigationSection,lightning:workspaceAPI,wave:waveDashboard'
+aura ='auraStorage:init,force:recordData,force:recordPreview,forceChatter:fullFeed,forceChatter:publisher,forceCommunity:appLauncher,forceCommunity:navigationMenuBase,forceCommunity:notifications,forceCommunity:routeLink,forceCommunity:waveDashboard'
 aura = aura.split(',')
 d = {}
 for item in aura:
-    url_key = ref_prefix + item.replace(':', '_')
+    if item == 'force:recordPreview':
+        url_key = 'data_service_force_record'
+    else:
+        url_key = ref_prefix + item.replace(':', '_')
     d[item] = get_attribute(url_key)
-with open('aura_attrs.json', 'w', encoding='utf-8') as aura_file:
+with open('force_attrs.json', 'w', encoding='utf-8') as aura_file:
     json.dump(d, aura_file, indent=4)
